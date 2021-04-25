1e020698b91b44f55dcf3855faacf9a90b8287c8
Waylon Walker <walker_waylon_s@cat.com>
2021-04-25 10:44:01 -0500

    FEAT add video to index

--- template/render.py
+++ template/render.py
@@ -5,12 +5,21 @@ with open("template/index.html") as f:
     index_template = Template(f.read())
 with open("template/image.html") as f:
     image_template = Template(f.read())
-images = "".join(
-    [
-        image_template.safe_substitute(file=file.name)
-        for file in Path("static/thumb").glob("*")
-    ]
-)
+with open("template/video.html") as f:
+    video_template = Template(f.read())
+
+
+items = [*list(Path("static/thumb").glob("*")), *list(Path("static").glob("*.webm"))]
+items.sort()
+
+
+def render(filepath):
+    if filepath.suffix == ".webm":
+        return video_template.safe_substitute(file=filepath.stem)
+    return image_template.safe_substitute(file=filepath.name)
+
+
+images = "".join([render(filepath) for filepath in items])
 
 index = index_template.safe_substitute(images=images)
 
